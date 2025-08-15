4e3c054faaf22dffc702f9ebe323c0da3124ca9a
Rosetejo18 <rosetejo2005@gmail.com>
2025-08-15 11:34:55 +0530

    added algos

--- BB.cpp
+++ BB.cpp
@@ -1,1 +1,84 @@
-// pending
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<stack>
+#include<algorithm>
+
+#define MAX_SIZE 7
+#define BEST_COST 11
+
+using namespace std;
+
+int adj[MAX_SIZE][MAX_SIZE] = {
+    // s a b c d e g
+    {0,3,5,0,0,0,0},
+    {3,0,4,0,5,0,0},
+    {5,4,0,4,0,0,0},
+    {0,0,4,0,0,6,0},
+    {0,5,0,0,0,0,3},
+    {0,0,0,6,0,0,0},
+    {0,0,0,0,3,0,0}
+};
+
+struct Node {
+    int nd;
+    int cost;
+    vector<int> path; //all parents in the corresponding branch
+
+    bool operator>(const Node &other) const {
+        return cost > other.cost;
+    }
+};
+
+void BB(int start, int goal) {
+    priority_queue<Node, vector<Node>, greater<Node>> pq;
+
+    pq.push({start, 0, {start}});
+    bool found  = false;
+
+    cout << "Traversal: ";
+    cout << start << " ";
+
+    while (!pq.empty()) {
+        Node node = pq.top();
+        pq.pop();
+
+        //cout << node.nd << " ";
+        if (node.nd == goal) {
+            found = true;
+            cout << "\nPath from " << start << " to " << goal << ": ";
+            for (int v : node.path) {
+                cout << v << " ";
+    
+            }
+            cout << "\nCost = " << node.cost;
+            break;
+        }
+
+        for (int i = 0; i < MAX_SIZE; i++) {
+            if (adj[node.nd][i] > 0 && find(node.path.begin(), node.path.end(), i) == node.path.end()) {
+                vector<int> newPath = node.path;
+                newPath.push_back(i);
+                int curCost = node.cost;
+                int adjCost = adj[node.nd][i];
+                int newCost = curCost + adjCost;
+                if (newCost <= BEST_COST){
+                    pq.push({i,newCost, newPath});
+                    cout << i << " ";
+                }
+            }
+        }
+    }
+
+    if (!found) {
+        cout << "No path found from " << start << " to " << goal << endl;
+    }
+    
+}
+
+int main() {
+    cout << "Branch and Bound with Oracle search: Start = 0 and Goal = 6\n";
+    BB(0, 6);
+    cout << endl;
+    return 0;
+}
